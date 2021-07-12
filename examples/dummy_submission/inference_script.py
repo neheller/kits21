974b225d169ab074257154c76b55078f407164fc
d246a <d.trofimova@dkfz-heidelberg.de>
2021-07-12 09:54:04 +0200

    dummy docker push test

--- examples/dummy_submission/inference_script.py
+++ examples/dummy_submission/inference_script.py
@@ -5,11 +5,15 @@ This python script should be able to load your pretrained model from the directo
 
 import os
 import numpy as np
+import nibabel as nib
 
 if not os.path.exists('/home/output'):
     os.mkdir('/home/output')
 
 for filename in os.listdir('/home/input'):
     if filename.endswith(".nii.gz"):
-        with open(os.path.join('/home/output/', filename.split('.')[0] + '.npy'), 'wb') as f:
-            np.save(f, np.array([1, 2]))
+        img = nib.load(os.path.join('/home/input', filename))
+        width, height, queue = img.dataobj.shape
+        data = np.arange(width * height * queue).reshape(width, height, queue)
+        img = nib.Nifti1Image(data, affine=np.eye(4))
+        nib.save(img, os.path.join('/home/output', filename))
